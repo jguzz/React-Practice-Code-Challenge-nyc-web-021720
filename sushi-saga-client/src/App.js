0dab19e7b6e429a2b297575cd5c51a843e363c7a
jguzz <joseph.a.guzzardo@gmail.com>
2020-05-18 10:20:03 -0400

    Deliverable 1 complete

--- sushi-saga-client/src/App.js
+++ sushi-saga-client/src/App.js
@@ -3,11 +3,22 @@ import SushiContainer from './containers/SushiContainer';
 import Table from './containers/Table';
 //JG
 // Endpoint!
-const API = "http://localhost:3000/sushis"
+const API = "http://localhost:3002/sushis"
 
 class App extends Component {
+  state = {
+    sushi: []
+  }
+
+  componentDidMount(){
+    //Deliverable 1
+    fetch(API)
+    .then( res => res.json())
+    .then( sushi => this.setState({sushi}))
+  }
 
   render() {
+    console.log(this.state.sushi)
     return (
       <div className="app">
         <SushiContainer />
