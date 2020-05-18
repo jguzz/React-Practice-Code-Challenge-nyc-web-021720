aa4e2f65eaab05bea0440796f8a22a88e96292ae
jguzz <joseph.a.guzzardo@gmail.com>
2020-05-18 10:06:12 -0400

    init commit

--- sushi-saga-client/src/App.js
+++ sushi-saga-client/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import SushiContainer from './containers/SushiContainer';
 import Table from './containers/Table';
-
+//JG
 // Endpoint!
 const API = "http://localhost:3000/sushis"
 
